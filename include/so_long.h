10b1177332fffe0b0c090a6581337553ffa54765
Jiraporn Jaroensutiyotin <jirapornjaroensutiyotin@Jiraporns-Laptop.local>
2024-05-09 22:10:40 +0700

    edit header

--- include/so_long.h
+++ include/so_long.h
@@ -6,30 +6,24 @@
 /*   By: jjaroens <jjaroens@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 17:03:28 by jjaroens          #+#    #+#             */
-/*   Updated: 2024/05/07 22:43:09 by jjaroens         ###   ########.fr       */
+/*   Updated: 2024/05/09 22:10:23 by jjaroens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
-// # define KEY_UP 65362
-// # define KEY_W 119
-// # define KEY_w 87
-// # define KEY_DOWN 65364
-// # define KEY_S 115
-// # define KEY_s 85
-// # define KEY_RIGHT 65363
-// # define KEY_D 100
-// # define KEY_d 68
-// # define KEY_LEFT 65361
-// # define KEY_A 65
-// # define KEY_a 97
-//////////// MAC /////
-# define KEY_W 3
-# define KEY_A 0
-# define KEY_S 2
-# define KEY_D 2
-# define ESC 53
+# define KEY_UP 65362
+# define KEY_W 119
+# define KEY_w 87
+# define KEY_DOWN 65364
+# define KEY_S 115
+# define KEY_s 85
+# define KEY_RIGHT 65363
+# define KEY_D 100
+# define KEY_d 68
+# define KEY_LEFT 65361
+# define KEY_A 65
+# define KEY_a 97
 # define IMG_SIZE 100 // to change in the code later
 
 // # include "../minilibx-linux/mlx.h"
